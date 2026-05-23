183e91bf26a2d313584b85728785b52b6f1252ff
Sanjaya Danushka <dsanjaya712@gmail.com>
2026-05-23 08:23:31 +0530

    main content implemeted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,27 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
+import { Metadata } from "next"
+
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: "GoPrompt",
+  description: "Discover, Create, and Share AI Prompts",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  // themeColor: "#ffffff",
+}
 
 export default function RootLayout({
   children,
@@ -20,10 +32,20 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main>
+          <ThemeProvider>{children}</ThemeProvider>
+        </main>
       </body>
     </html>
   )
